a7d63b7bc717c3b388768134a1247c85765d0fdc
= <=>
2023-08-02 23:09:54 +0700

    update cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://be-whotalkingchat.vercel.app",
     }
 })
 global.onlineUsers = new Map();
